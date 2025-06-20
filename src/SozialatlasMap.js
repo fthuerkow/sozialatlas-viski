40a92ece85aae412ccd77d1d661e59f6c703abc8
Florian Thuerkow <florian.thuerkow@gmail.com>
2025-06-20 15:57:56 +0200

    added osm geojson san

--- src/SozialatlasMap.js
+++ src/SozialatlasMap.js
@@ -1,73 +1,51 @@
-// Sozialatlas Frontend-Prototyp (React + MapLibre)
-import React, { useEffect, useRef } from "react";
-import maplibre from "maplibre-gl";
-import "maplibre-gl/dist/maplibre-gl.css";
+import { useEffect, useState } from "react";
+import maplibregl from "maplibre-gl";
 
-const SozialatlasMap = () => {
-  const mapContainer = useRef(null);
+export default function SozialatlasMap() {
+  const [map, setMap] = useState(null);
 
   useEffect(() => {
-    const map = new maplibre.Map({
-      container: mapContainer.current,
+    const mapInstance = new maplibregl.Map({
+      container: "map",
       style: "https://demotiles.maplibre.org/style.json",
-      center: [11.8, 51.8],
-      zoom: 7
+      center: [11.97, 51.47], // Halle
+      zoom: 11,
     });
 
-    map.on("load", () => {
-      map.addSource("kreise", {
-        type: "geojson",
-        data: "/data/sozialatlas_prototyp.geojson"
-      });
+    mapInstance.on("load", () => {
+      fetch("/data/export.geojson")
+        .then((res) => res.json())
+        .then((geojson) => {
+          mapInstance.addSource("halle", {
+            type: "geojson",
+            data: geojson,
+          });
 
-      map.addLayer({
-        id: "kreise-fill",
-        type: "fill",
-        source: "kreise",
-        paint: {
-          "fill-color": [
-            "match",
-            ["get", "Cluster"],
-            "A", "#a6cee3",
-            "B", "#1f78b4",
-            "C", "#03396c",
-            "#ccc"
-          ],
-          "fill-opacity": 0.6
-        }
-      });
+          mapInstance.addLayer({
+            id: "halle-fill",
+            type: "fill",
+            source: "halle",
+            paint: {
+              "fill-color": "#088",
+              "fill-opacity": 0.4,
+            },
+          });
 
-      map.addLayer({
-        id: "kreise-outline",
-        type: "line",
-        source: "kreise",
-        paint: {
-          "line-color": "#333",
-          "line-width": 1
-        }
-      });
-
-      map.on("click", "kreise-fill", (e) => {
-        const props = e.features[0].properties;
-        const popupHtml = `
-          <strong>${props.Kreis}</strong><br/>
-          Cluster: ${props.Cluster}<br/>
-          Bevölkerung: ${props.Bevölkerung}<br/>
-          Anteil 65+: ${props.Anteil_65plus}%<br/>
-          Arbeitslosigkeit: ${props.Arbeitslosenquote}%<br/>
-          Migrationsanteil: ${props.Migrationsanteil}%<br/>
-          Hochschulabschluss: ${props.Hochschulabschlussquote}%
-        `;
-
-        new maplibre.Popup()
-          .setLngLat(e.lngLat)
-          .setHTML(popupHtml)
-          .addTo(map);
-      });
+          mapInstance.addLayer({
+            id: "halle-outline",
+            type: "line",
+            source: "halle",
+            paint: {
+              "line-color": "#000",
+              "line-width": 1,
+            },
+          });
+        });
     });
+
+    setMap(mapInstance);
+    return () => mapInstance.remove();
   }, []);
 
-  return <div ref={mapContainer} style={{ height: "100vh" }} />;
-};
-
-export default SozialatlasMap;
+  return <div id="map" style={{ width: "100%", height: "100vh" }} />;
+}
